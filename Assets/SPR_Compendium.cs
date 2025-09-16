6dc879a0cd0eacd3a1b3d5b5ae56a43a56958e7a
Joe Orchard <JO278870@falmouth.ac.uk>
2025-09-16 20:06:53 +0100

    Started on compendium
    Need to code logic for first time picking up fruit to add it to the compendium

---
+++ Assets/SPR_Compendium.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SPR_Compendium : MonoBehaviour, INT_Interactable
+{
+    public void Interact(GameObject interactor)
+    {
+        Debug.Log("Interacting with compendium");
+    }
+}
